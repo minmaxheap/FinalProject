b148125a6c668b4491ebfd6b65e5136cd23b9189
minmaxheap <minmaxheap@naver.com>
2021-12-19 18:47:48 +0900

    유틸 추가

---
+++ MES_Team3/Utility/DataGridViewUtil.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MES_Team3
+{
+    class DataGridViewUtil
+    {
+        public static void SetInitGridView(DataGridView dgv)
+        {
+            dgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgv.AllowUserToAddRows = false;
+            dgv.AutoGenerateColumns = false;
+            dgv.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgv.RowHeadersVisible = false;
+        }
+
+        public static void AddGridTextColumn(DataGridView dgv,
+            string headerText,
+            string propertyName,
+             DataGridViewContentAlignment align = DataGridViewContentAlignment.MiddleLeft,
+            int width = 100,
+            bool visibility = true,
+            bool frozen = false)
+        {
+            DataGridViewTextBoxColumn col = new DataGridViewTextBoxColumn();
+            col.HeaderText = headerText;
+            col.Name = propertyName;
+            
+            col.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            col.DataPropertyName = propertyName;
+            col.DefaultCellStyle.Alignment = align;
+            col.Width = width;
+            col.Visible = visibility;
+            col.Frozen = frozen;
+            col.ReadOnly = true;
+            dgv.Columns.Add(col);
+        }
+    }
+}
