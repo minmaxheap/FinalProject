44d07133d46eb405237808f586f9c2f1f72bffba
anto4531 <anto4531@naver.com>
2021-12-23 14:05:17 +0900

    품질설정폼 완성

---
+++ FinalProject/MES_Team3/Services/InspecServ.cs
@@ -0,0 +1,22 @@
+﻿using DAC;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VO;
+
+namespace MES_Team3.Services
+{
+	public class InspecServ
+	{
+		public DataTable GetInsMst()
+		{
+			InspectDAC db = new InspectDAC();
+			DataTable dt = db.GetInsMst();
+			db.Dispose();
+			return dt;
+		}
+	}
+}
