0f6cdc290f01ba66c2d4f250dc98e14b385725b4
건희 장 <jangwh9466@gmail.com>
2021-12-30 13:41:47 +0900

    ㅇㅇ

---
+++ FinalProject/MES_Team3/frmBOM.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace MES_Team3
+{
+    partial class frmBOM
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pgSearch = new System.Windows.Forms.PropertyGrid();
+            this.pgProperty = new System.Windows.Forms.PropertyGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.searchPanel.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel9.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.pnlCrud.SuspendLayout();
+            this.pnlTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            // 
+            // splitContainer2
+            // 
+            // 
+            // splitContainer3
+            // 
+            // 
+            // searchPanel
+            // 
+            this.searchPanel.Controls.Add(this.pgSearch);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pgProperty);
+            this.pnlTop.Controls.SetChildIndex(this.btnSearchPnl, 0);
+            this.pnlTop.Controls.SetChildIndex(this.txtSearch, 0);
+            // 
+            // pgSearch
+            // 
+            this.pgSearch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pgSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.pgSearch.HelpVisible = false;
+            this.pgSearch.Location = new System.Drawing.Point(0, 0);
+            this.pgSearch.Name = "pgSearch";
+            this.pgSearch.Size = new System.Drawing.Size(357, 219);
+            this.pgSearch.TabIndex = 2;
+            this.pgSearch.ToolbarVisible = false;
+            // 
+            // pgProperty
+            // 
+            this.pgProperty.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pgProperty.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.pgProperty.HelpVisible = false;
+            this.pgProperty.Location = new System.Drawing.Point(0, 0);
+            this.pgProperty.Name = "pgProperty";
+            this.pgProperty.Size = new System.Drawing.Size(502, 216);
+            this.pgProperty.TabIndex = 2;
+            this.pgProperty.ToolbarVisible = false;
+            // 
+            // frmBOM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.ClientSize = new System.Drawing.Size(1168, 667);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "frmBOM";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.searchPanel.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel9.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.pnlCrud.ResumeLayout(false);
+            this.pnlTop.ResumeLayout(false);
+            this.pnlTop.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PropertyGrid pgSearch;
+        private System.Windows.Forms.PropertyGrid pgProperty;
+    }
+}
