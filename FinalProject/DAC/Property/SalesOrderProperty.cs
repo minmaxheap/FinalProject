7377bb769f19e62ad946bc3cb6a80eef145e51d7
기문 송 <octodin1@gmail.com>
2022-01-02 21:42:17 +0900

    주문 생성

---
+++ FinalProject/DAC/Property/SalesOrderProperty.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DAC
+{
+    public class SalesOrderProperty
+    {
+        private DateTime order_date;
+        private string sales_order_id;
+        private string customer_code;
+        private string product_code;
+        private int order_qty;
+        private string confirm_flag;
+        private string ship_flag;
+
+        private DateTime create_time;
+        private string create_user_id;
+        private DateTime update_time;
+        private string update_user_id;
+
+        [System.ComponentModel.RefreshProperties(RefreshProperties.All)]
+        [DisplayName("작업일자")]
+        public DateTime ORDER_DATE { get { return order_date; } set { order_date = value; } }
+
+        [DisplayName("주문서코드")]
+        public string SALES_ORDER_ID { get { return sales_order_id; } set { sales_order_id = value; } }
+
+        [DisplayName("고객사")]
+        [TypeConverter(typeof(CustomerCodeConverter))]
+        public string CUSTOMER_CODE { get { return customer_code; }set { customer_code = value; }}
+
+        [DisplayName("품번")]
+        [TypeConverter(typeof(Get_Product_CodeList_Converter))]
+        public string PRODUCT_CODE { get { return product_code; } set { product_code = value; } }
+
+        [DisplayName("주문수량")]
+        public int ORDER_QTY { get { return order_qty; } set { order_qty = value; } }
+
+        [DisplayName("확정여부")]
+        [TypeConverter(typeof(Check_Confirm_YN_Converter))]
+        public string CONFIRM_FLAG { get { return confirm_flag; } set { confirm_flag = value; } }
+
+        [DisplayName("출하여부")]
+        [TypeConverter(typeof(Check_Confirm_YN_Converter))]
+        public string SHIP_FLAG { get { return ship_flag; } set { ship_flag = value; } }
+
+
+        [ReadOnly(true)]
+        [DisplayName("생성 시간")]
+        public DateTime CREATE_TIME { get { return create_time; } set { create_time = value; } }
+
+        [ReadOnly(true)]
+        [DisplayName("생성 사용자")]
+        public string CREATE_USER_ID { get { return create_user_id; } set { create_user_id = value; } }
+
+        [ReadOnly(true)]
+        [DisplayName("변경 시간")]
+        public DateTime UPDATE_TIME { get { return update_time; } set { update_time = value; } }
+
+        [ReadOnly(true)]
+        [DisplayName("변경 사용자")]
+        public string UPDATE_USER_ID { get { return update_user_id; } set  {update_user_id = value;} }
+
+        public SalesOrderProperty()
+        {
+
+         }
+
+    }
+    public class SalesOrderPropertySch
+    {
+        private DateTime search_start_date;
+        private DateTime search_end_date;
+        private string sales_order_id;
+        private string customer_code;
+        private string product_code;
+        private string confirm_check;
+
+        [System.ComponentModel.RefreshProperties(RefreshProperties.All)]
+        [DisplayName("조회 시작 일자")]
+        public DateTime SEARCH_START_DATE { get { return search_start_date; } set { search_start_date = value; } }
+
+        [DisplayName("조회 종료 일자")]
+        public DateTime SEARCH_END_DATE { get { return search_end_date; } set { search_end_date = value; } }
+
+        [DisplayName("주문서코드")]
+        public string SALES_ORDER_ID { get { return sales_order_id; } set { sales_order_id = value; } }
+
+        [DisplayName("고객사")]
+        [TypeConverter(typeof(CustomerCodeConverter))]
+        public string CUSTOMER_CODE { get { return customer_code; } set { customer_code = value; } }
+
+        [DisplayName("품번")]
+        [TypeConverter(typeof(Get_Product_CodeList_Converter))]
+        public string PRODUCT_CODE { get { return product_code; } set { product_code = value; } }
+
+        [DisplayName("확정여부")]
+        [TypeConverter(typeof(Check_Confirm_Converter))]
+        public string CONFIRM_CHECK { get { return confirm_check; } set { confirm_check = value; } }
+
+
+
+
+        public SalesOrderPropertySch()
+        {
+
+        }
+
+    }
+}
---
+++ FinalProject/MES_Team3/frmSalesOrder.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace MES_Team3
+{
+    partial class frmSalesOrder
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.pgProperty = new System.Windows.Forms.PropertyGrid();
+            this.pgSearch = new System.Windows.Forms.PropertyGrid();
+            this.csDataGridView1 = new MES_Team3.csDataGridView();
+            this.pnlTop.SuspendLayout();
+            this.pnlProperty.SuspendLayout();
+            this.pnlSearch.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.spcBase)).BeginInit();
+            this.spcBase.Panel1.SuspendLayout();
+            this.spcBase.Panel2.SuspendLayout();
+            this.spcBase.SuspendLayout();
+            this.pnlDgv.SuspendLayout();
+            this.pnlCrud.SuspendLayout();
+            this.pnlTopLbl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.csDataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Text = "작업지시 목록";
+            // 
+            // pnlProperty
+            // 
+            this.pnlProperty.Controls.Add(this.pgProperty);
+            // 
+            // pnlSearch
+            // 
+            this.pnlSearch.Controls.Add(this.pgSearch);
+            // 
+            // btnPanel
+            // 
+            this.btnPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(152)))), ((int)(((byte)(216)))));
+            this.btnPanel.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(220)))), ((int)(((byte)(229)))));
+            this.btnPanel.FlatAppearance.BorderSize = 0;
+            this.btnPanel.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(243)))), ((int)(((byte)(251)))));
+            this.btnPanel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(243)))), ((int)(((byte)(251)))));
+            this.btnPanel.Click += new System.EventHandler(this.btnPanel_Click);
+            // 
+            // spcBase
+            // 
+            // 
+            // pnlDgv
+            // 
+            this.pnlDgv.Controls.Add(this.csDataGridView1);
+            // 
+            // btnSearchPnl
+            // 
+            this.btnSearchPnl.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSearch_KeyDown);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Size = new System.Drawing.Size(63, 48);
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Size = new System.Drawing.Size(63, 48);
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Size = new System.Drawing.Size(63, 48);
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Size = new System.Drawing.Size(63, 48);
+            // 
+            // btnReadBottom
+            // 
+            this.btnReadBottom.Size = new System.Drawing.Size(63, 48);
+            this.btnReadBottom.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Size = new System.Drawing.Size(63, 48);
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnTxtSearch
+            // 
+            this.btnTxtSearch.Click += new System.EventHandler(this.btnTxtSearch_Click);
+            // 
+            // btnReadTop
+            // 
+            this.btnReadTop.Click += new System.EventHandler(this.btnReadTop_Click);
+            // 
+            // pgProperty
+            // 
+            this.pgProperty.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pgProperty.HelpVisible = false;
+            this.pgProperty.Location = new System.Drawing.Point(0, 0);
+            this.pgProperty.Name = "pgProperty";
+            this.pgProperty.Size = new System.Drawing.Size(362, 591);
+            this.pgProperty.TabIndex = 4;
+            this.pgProperty.ToolbarVisible = false;
+            // 
+            // pgSearch
+            // 
+            this.pgSearch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pgSearch.HelpVisible = false;
+            this.pgSearch.Location = new System.Drawing.Point(0, 0);
+            this.pgSearch.Name = "pgSearch";
+            this.pgSearch.Size = new System.Drawing.Size(362, 591);
+            this.pgSearch.TabIndex = 3;
+            this.pgSearch.ToolbarVisible = false;
+            // 
+            // csDataGridView1
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(239)))), ((int)(((byte)(247)))));
+            this.csDataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.csDataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(243)))), ((int)(((byte)(251)))));
+            this.csDataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(220)))), ((int)(((byte)(229)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("나눔고딕", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(220)))), ((int)(((byte)(229)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.csDataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.csDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("나눔고딕", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(195)))), ((int)(((byte)(230)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.csDataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.csDataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.csDataGridView1.EnableHeadersVisualStyles = false;
+            this.csDataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.csDataGridView1.Name = "csDataGridView1";
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("나눔고딕", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.ControlDark;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.csDataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.csDataGridView1.RowHeadersWidth = 30;
+            this.csDataGridView1.RowTemplate.Height = 23;
+            this.csDataGridView1.Size = new System.Drawing.Size(794, 597);
+            this.csDataGridView1.TabIndex = 0;
+            this.csDataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.csDataGridView1_CellClick);
+            // 
+            // frmSalesOrder
+            // 
+            this.BIsSearchPanel = true;
+            this.BPropertyPanel = true;
+            this.BSearchPanel = true;
+            this.ClientSize = new System.Drawing.Size(1168, 778);
+            this.Name = "frmSalesOrder";
+            this.Text = "품번 설정";
+            this.Load += new System.EventHandler(this.frmProduct1_Load);
+            this.pnlTop.ResumeLayout(false);
+            this.pnlTop.PerformLayout();
+            this.pnlProperty.ResumeLayout(false);
+            this.pnlSearch.ResumeLayout(false);
+            this.spcBase.Panel1.ResumeLayout(false);
+            this.spcBase.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.spcBase)).EndInit();
+            this.spcBase.ResumeLayout(false);
+            this.pnlDgv.ResumeLayout(false);
+            this.pnlCrud.ResumeLayout(false);
+            this.pnlTopLbl.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.csDataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PropertyGrid pgProperty;
+        private System.Windows.Forms.PropertyGrid pgSearch;
+        private csDataGridView csDataGridView1;
+    }
+}
