455fce2d067599bde80a26410117e536c831b3bd
anto4531 <anto4531@naver.com>
2022-01-03 16:24:17 +0900

    건희(StockDAC) 완성!!!!

---
+++ POPprogram/DAC/StockDAC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAC
+{
+	public class StockDAC : IDisposable
+	{
+		SqlConnection conn;
+        public StockDAC()
+        {
+            //conn = new SqlConnection(ConfigurationManager.ConnectionStrings["local"].ConnectionString);
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["project"].ConnectionString);
+            conn.Open();
+
+        }
+
+        public void Dispose()
+        {
+            conn.Close();
+        }
+    }
+}
