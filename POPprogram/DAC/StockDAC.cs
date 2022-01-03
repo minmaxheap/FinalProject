f8f48adeb89e0192511fd62e28ea94fbeb777835
anto4531 <anto4531@naver.com>
2022-01-03 18:10:19 +0900

    stock 커밋 동기화

--- POPprogram/DAC/StockDAC.cs
+++ POPprogram/DAC/StockDAC.cs
@@ -10,9 +10,9 @@ using System.Threading.Tasks;
 
 namespace DAC
 {
-	public class StockDAC : IDisposable
-	{
-		SqlConnection conn;
+    public class StockDAC : IDisposable
+    {
+        SqlConnection conn;
         public StockDAC()
         {
             //conn = new SqlConnection(ConfigurationManager.ConnectionStrings["local"].ConnectionString);
@@ -25,5 +25,26 @@ namespace DAC
         {
             conn.Close();
         }
+
+        public DataTable Purchase_warehousing(string Code)
+        {
+            string sql = @" select ROW_NUMBER() over(order by(select 1)) as RowNum,b.CHILD_PRODUCT_CODE,p.PRODUCT_NAME, b.REQUIRE_QTY, (b.REQUIRE_QTY *po.ORDER_QTY) as 수량 ,po.STOCK_IN_LOT_ID
+  from	[dbo].[PURCHASE_ORDER_MST] po 
+  left join [dbo].[PRODUCT_MST] p on po.MATERIAL_CODE = p.PRODUCT_CODE
+  left join [dbo].[BOM_MST] b on p.PRODUCT_CODE = b.PRODUCT_CODE
+   where po.PURCHASE_ORDER_ID =@po.PURCHASE_ORDER_ID";
+
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            {
+                cmd.Parameters.AddWithValue("@po.PURCHASE_ORDER_ID", Code);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+
+            }
+
+        }
     }
 }
+
