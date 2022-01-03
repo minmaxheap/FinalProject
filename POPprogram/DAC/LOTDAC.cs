1d278623a5ff07fec4f18c86d4b749c5f024a2b9
minmaxheap <minmaxheap@naver.com>
2022-01-03 16:34:47 +0900

    .

--- POPprogram/DAC/LOTDAC.cs
+++ POPprogram/DAC/LOTDAC.cs
@@ -27,19 +27,7 @@ namespace DAC
             conn.Close();
         }
 
-        public DataTable GetPurchaseList()
-        {
-            string sql = @"select PURCHASE_ORDER_ID, SALES_ORDER_ID, ORDER_DATE, O.VENDOR_CODE,V.DATA_1 VENDOR_NAME, MATERIAL_CODE,  P.PRODUCT_NAME PRODUCT_NAME,ORDER_QTY, STOCK_IN_FLAG, STOCK_IN_STORE_CODE, STOCK_IN_LOT_ID
-from [dbo].[PURCHASE_ORDER_MST] O, PRODUCT_MST P, CODE_DATA_MST V
-WHERE  P.PRODUCT_CODE = O.MATERIAL_CODE AND V.CODE_TABLE_NAME ='CM_VENDOR' AND V.KEY_1=O.VENDOR_CODE
-";
-            DataTable dt = new DataTable();
-            using (SqlDataAdapter da = new SqlDataAdapter(sql, conn))
-            {
-                da.Fill(dt);
-                return dt;
-            }
-        }
+
 
 
 
