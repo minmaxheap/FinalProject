ea2368e561dfd957e7520569ab6466835d1206c6
minmaxheap <minmaxheap@naver.com>
2022-01-08 22:17:13 +0900

    참고 코드 정리

---
+++ NiceWEB/NiceWEB/Models/DAC/EfficiencyDAC.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace NiceWEB.Models
+{
+    public class EfficiencyDAC:IDisposable
+    {
+        SqlConnection conn;
+        public EfficiencyDAC()
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["project"].ConnectionString);
+            conn.Open();
+        }
+
+        public void Dispose()
+        {
+            conn.Close();
+        }
+
+        public DataTable GetData(string from, string to)
+        {
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.Connection = new SqlConnection(ConfigurationManager.ConnectionStrings["project"].ConnectionString);
+                cmd.CommandText = @"SELECT  CONVERT(DATE,W.ORDER_DATE),ORDER_QTY,  PRODUCT_QTY, DEFECT_QTY,
+cast((PRODUCT_QTY/(PRODUCT_QTY+DEFECT_QTY))*100.0 as decimal(4,2)) as QUALITY_RATE,cast( (DEFECT_QTY/(PRODUCT_QTY+DEFECT_QTY))*100 as decimal(4,2)) AS DEFECT_RATE, 
+CONVERT(TIME, WORK_START_TIME) AS WORK_START_TIME,CONVERT(TIME, WORK_CLOSE_TIME) AS WORK_CLOSE_TIME, T.WORK_TIME, SUM(DT_TIME),
+ cast((WORK_TIME/(WORK_TIME+H.DT_TIME))*100.0 as decimal(4,2)) as WORK_RATE,cast((H.DT_TIME/(WORK_TIME+H.DT_TIME))*100 as decimal(4,2)) AS DOWN_RATE, 
+(PRODUCT_QTY+DEFECT_QTY)/(WORK_TIME+H.DT_TIME) AS PER_TIME
+FROM [dbo].[WORK_ORDER_MST] W, EQUIP_DOWN_HIS H, 
+(select ORDER_DATE, DATEDIFF(MINUTE,WORK_START_TIME, WORK_CLOSE_TIME) WORK_TIME from WORK_ORDER_MST WHERE ORDER_STATUS = 'CLOSE' ) AS T
+WHERE W.ORDER_STATUS = 'CLOSE' AND CONVERT(DATE,W.ORDER_DATE) = CONVERT(DATE,H.DT_DATE) AND CONVERT(DATE,T.ORDER_DATE) = CONVERT(DATE,W.ORDER_DATE)
+GROUP BY W.ORDER_DATE,ORDER_QTY,  PRODUCT_QTY, DEFECT_QTY,WORK_START_TIME,WORK_CLOSE_TIME,T.WORK_TIME";
+                cmd.Parameters.AddWithValue("@from", from);
+                cmd.Parameters.AddWithValue("@to", to);
+
+                DataTable dt = new DataTable();
+                cmd.Connection.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                cmd.Connection.Close();
+
+
+
+                return dt;
+            }
+        }
+    }
+}
---
+++ NiceWEB/NiceWEB/Models/DAC/InspectDAC.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace NiceWEB.Models
+{
+    public class InspectDAC : IDisposable
+    {
+        SqlConnection conn;
+        public InspectDAC()
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["project"].ConnectionString);
+            conn.Open();
+        }
+
+        public void Dispose()
+        {
+            conn.Close();
+        }
+
+        public DataTable GetData(string from, string to)
+        {
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.Connection = new SqlConnection(ConfigurationManager.ConnectionStrings["project"].ConnectionString);
+                cmd.CommandText = @" SELECT TRAN_TIME,LOT_ID,  H.PRODUCT_CODE, PRODUCT_NAME, H.OPERATION_CODE, OPERATION_NAME, INSPECT_ITEM_CODE, INSPECT_ITEM_NAME, VALUE_TYPE, 
+ SPEC_LSL, SPEC_TARGET, SPEC_USL, INSPECT_VALUE,  TRAN_CODE,TRAN_USER_ID
+ FROM [dbo].[LOT_INSPECT_HIS] H, PRODUCT_MST P, OPERATION_MST O
+ WHERE H.PRODUCT_CODE = P.PRODUCT_CODE AND H.OPERATION_CODE = O.OPERATION_CODE
+ ORDER BY TRAN_TIME, LOT_ID";
+                cmd.Parameters.AddWithValue("@from", from);
+                cmd.Parameters.AddWithValue("@to", to);
+
+                DataTable dt = new DataTable();
+                cmd.Connection.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                cmd.Connection.Close();
+
+
+
+                return dt;
+            }
+        }
+    
+    }
+}
