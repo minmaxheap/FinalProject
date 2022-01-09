e7b4ab742f034e44897d6a5a954bcd1aab22ed55
minmaxheap <minmaxheap@naver.com>
2022-01-09 19:37:50 +0900

    웹

--- NiceWEB/NiceWEB/Models/DAC/ComparePlanDAC.cs
+++ NiceWEB/NiceWEB/Models/DAC/ComparePlanDAC.cs
@@ -29,14 +29,52 @@ namespace NiceWEB.Models
             using (SqlCommand cmd = new SqlCommand())
             {
                 cmd.Connection = new SqlConnection(ConfigurationManager.ConnectionStrings["project"].ConnectionString);
-                cmd.CommandText = @"SELECT ORDER_DATE,WORK_ORDER_ID,  W.PRODUCT_CODE, P.PRODUCT_NAME, 
+                cmd.CommandText = @"SELECT CONVERT(varchar, CONVERT(DATE, ORDER_DATE)) ORDER_DATE,WORK_ORDER_ID,  W.PRODUCT_CODE, P.PRODUCT_NAME, 
 ORDER_QTY,  PRODUCT_QTY, DEFECT_QTY,
-cast((PRODUCT_QTY/(PRODUCT_QTY+DEFECT_QTY))*100.0 as decimal(4,2)) as QUALITY_RATE,cast( (DEFECT_QTY/(PRODUCT_QTY+DEFECT_QTY))*100 as decimal(4,2)) AS DEFECT_RATE, WORK_CLOSE_TIME
+cast((PRODUCT_QTY/(PRODUCT_QTY+DEFECT_QTY))*100.0 as decimal(4,2)) as QUALITY_RATE,cast( (DEFECT_QTY/(PRODUCT_QTY+DEFECT_QTY))*100 as decimal(4,2)) AS DEFECT_RATE, CONVERT(varchar, WORK_CLOSE_TIME) WORK_CLOSE_TIME
 FROM [dbo].[WORK_ORDER_MST] W, PRODUCT_MST P
-WHERE W.PRODUCT_CODE = P.PRODUCT_CODE ";
+WHERE W.PRODUCT_CODE = P.PRODUCT_CODE";
                 cmd.Parameters.AddWithValue("@from", from);
                 cmd.Parameters.AddWithValue("@to", to);
 
+                DataTable dt = new DataTable();
+                cmd.Connection.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                cmd.Connection.Close();
+
+
+
+                return dt;
+            }
+        }
+
+        public DataTable GetWorkOrder()
+        {
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.Connection = new SqlConnection(ConfigurationManager.ConnectionStrings["project"].ConnectionString);
+                cmd.CommandText = @"SELECT WORK_ORDER_ID
+FROM [dbo].[WORK_ORDER_MST]";
+                DataTable dt = new DataTable();
+                cmd.Connection.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                cmd.Connection.Close();
+
+
+
+                return dt;
+            }
+        }
+
+        public DataTable GetProductCode()
+        {
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.Connection = new SqlConnection(ConfigurationManager.ConnectionStrings["project"].ConnectionString);
+                cmd.CommandText = @"SELECT DISTINCT PRODUCT_CODE
+FROM [dbo].[WORK_ORDER_MST]";
                 DataTable dt = new DataTable();
                 cmd.Connection.Open();
                 SqlDataAdapter da = new SqlDataAdapter(cmd);
