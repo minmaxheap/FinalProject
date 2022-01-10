e09b3587d94243e1ef0b0b5f33c513fd9f8567fa
anto4531 <anto4531@naver.com>
2022-01-10 13:50:09 +0900

    자재투입이력조회 (controller,dac.view 생성)

---
+++ NiceWEB/NiceWEB/Controllers/Adding_materialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NiceWEB.Controllers
+{
+    public class Adding_materialController : Controller
+    {
+        // GET: Adding_material
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
