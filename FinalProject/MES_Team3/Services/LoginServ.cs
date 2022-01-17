1249d204a14b83f5ea0d10e555a708e71ecdfba7
기문 송 <octodin1@gmail.com>
2022-01-17 14:25:58 +0900

    로그인 적용

---
+++ FinalProject/MES_Team3/Services/LoginServ.cs
@@ -0,0 +1,22 @@
+﻿using DAC;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MES_Team3
+{
+    public class LoginServ
+	{
+		public bool CheckLogin(LoginProperty vo)
+		{
+			LoginDAC dac = new LoginDAC();
+			bool bResult = dac.CheckLogin(vo);
+			dac.Dispose();
+			return bResult;
+		}
+	}
+}
+
