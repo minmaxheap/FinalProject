261e58c2de8950c4f4f86068533703908c38fd78
기문 송 <octodin1@gmail.com>
2022-01-25 16:26:04 +0900

    속성창 크기 변경

--- FinalProject/MES_Team3/frmOperation.Designer.cs
+++ FinalProject/MES_Team3/frmOperation.Designer.cs
@@ -51,23 +51,23 @@ namespace MES_Team3
             // 
             // lblTitle
             // 
-            this.lblTitle.Size = new System.Drawing.Size(777, 55);
+            this.lblTitle.Size = new System.Drawing.Size(852, 55);
             this.lblTitle.Text = "공정 목록";
             // 
             // pnlProperty
             // 
             this.pnlProperty.Controls.Add(this.pgProperty);
-            this.pnlProperty.Size = new System.Drawing.Size(359, 542);
+            this.pnlProperty.Size = new System.Drawing.Size(284, 542);
             // 
             // lblPanel
             // 
-            this.lblPanel.Size = new System.Drawing.Size(359, 55);
+            this.lblPanel.Size = new System.Drawing.Size(284, 55);
             // 
             // pnlSearch
             // 
             this.pnlSearch.Controls.Add(this.pgSearch);
             this.pnlSearch.Font = new System.Drawing.Font("나눔고딕", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.pnlSearch.Size = new System.Drawing.Size(359, 542);
+            this.pnlSearch.Size = new System.Drawing.Size(284, 542);
             // 
             // btnPanel
             // 
@@ -76,20 +76,25 @@ namespace MES_Team3
             this.btnPanel.FlatAppearance.BorderSize = 0;
             this.btnPanel.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(243)))), ((int)(((byte)(251)))));
             this.btnPanel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(243)))), ((int)(((byte)(251)))));
-            this.btnPanel.Location = new System.Drawing.Point(305, 10);
+            this.btnPanel.Location = new System.Drawing.Point(230, 10);
             this.btnPanel.Click += new System.EventHandler(this.btnPanel_Click);
             // 
             // spcBase
             // 
-            this.spcBase.SplitterDistance = 779;
+            this.spcBase.SplitterDistance = 854;
             // 
             // pnlDgv
             // 
             this.pnlDgv.Controls.Add(this.csDataGridView1);
+            this.pnlDgv.Size = new System.Drawing.Size(852, 542);
             // 
             // btnSearchPnl
             // 
             this.btnSearchPnl.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pnlTopLbl
+            // 
+            this.pnlTopLbl.Size = new System.Drawing.Size(284, 55);
             // 
             // txtSearch
             // 
@@ -134,7 +139,7 @@ namespace MES_Team3
             this.pgProperty.HelpVisible = false;
             this.pgProperty.Location = new System.Drawing.Point(0, 0);
             this.pgProperty.Name = "pgProperty";
-            this.pgProperty.Size = new System.Drawing.Size(357, 540);
+            this.pgProperty.Size = new System.Drawing.Size(282, 540);
             this.pgProperty.TabIndex = 4;
             this.pgProperty.ToolbarVisible = false;
             // 
@@ -145,7 +150,7 @@ namespace MES_Team3
             this.pgSearch.HelpVisible = false;
             this.pgSearch.Location = new System.Drawing.Point(0, 0);
             this.pgSearch.Name = "pgSearch";
-            this.pgSearch.Size = new System.Drawing.Size(357, 540);
+            this.pgSearch.Size = new System.Drawing.Size(282, 540);
             this.pgSearch.TabIndex = 3;
             this.pgSearch.ToolbarVisible = false;
             // 
@@ -186,7 +191,7 @@ namespace MES_Team3
             dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.csDataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.csDataGridView1.RowHeadersWidth = 30;
-            this.csDataGridView1.Size = new System.Drawing.Size(777, 542);
+            this.csDataGridView1.Size = new System.Drawing.Size(852, 542);
             this.csDataGridView1.TabIndex = 0;
             this.csDataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.csDataGridView1_CellClick);
             // 
